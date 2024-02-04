74d285b15159d9544d4b260f10b2dc23e17dd5a4
A-Yamout <70601578+A-Yamout@users.noreply.github.com>
2024-02-04 22:39:08 +0300

    Revert "Tracking"
    This reverts commit 34b44db33c288bf5a33f21b2bd6321ff58feb3ea.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,6 @@
 import type { AppProps } from 'next/app'
 import '../styles/index.css'
-import { Analytics } from '@vercel/analytics/react';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <Component {...pageProps} />
-      <Analytics />
-    </>
-  );
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />
 }
- 
-export default MyApp;
