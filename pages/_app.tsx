34b44db33c288bf5a33f21b2bd6321ff58feb3ea
A-Yamout <70601578+A-Yamout@users.noreply.github.com>
2024-02-04 14:02:44 +0300

    Tracking

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
 import type { AppProps } from 'next/app'
 import '../styles/index.css'
+import { Analytics } from '@vercel/analytics/react';
 
-export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 }
+ 
+export default MyApp;
